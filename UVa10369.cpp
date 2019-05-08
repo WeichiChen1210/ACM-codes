4278f11067e3a50779004c185ece01fa6afa3479
WeichiChen1210 <iphone31302@gmail.com>
2019-05-08 17:44:54 +0800

    practices

--- UVa10369.cpp
+++ UVa10369.cpp
@@ -14,12 +14,12 @@ struct edge{
     // bool operator<(const node &rhs) const{
     //     return w > rhs.w;   // make priority_queue be min heap
     // }
-};
+} E[10000];
 
 struct coor{
     int x;
     int y;
-};
+} C[10000];
 
 int group[10000];
 
@@ -42,25 +42,26 @@ int main(){
 
     int n, s, p;
     cin >> n;
-    vector<struct edge> E;
-    vector<struct coor> C;
+    // vector<struct edge> E;
+    // vector<struct coor> C;
 
     while(n > 0){
-        cin >> s >> p;
-        for(int v = 0; v < p; v++){
-            struct coor temp;
-            scanf("%d %d", &temp.x, &temp.y);
-            C.push_back(temp);
-            group[v] = v;
+        scanf("%d %d", &s, &p);
+        for(int i = 0; i < p; i++){
+            scanf("%d %d", &C[i].x, &C[i].y);
+            group[i] = i;
         }
+        cout << "first\n";
+        int count = 0;
         for(int v = 0; v < p; v++){
             for(int u = v+1; u < v; u++){
                 double dist = sqrt(pow(C[u].x-C[v].x, 2)+pow(C[u].y-C[v].y, 2));
-                E.push_back({u, v, dist});
+                E[count++] = {u, v, dist};
             }
         }
+        cout << "before sort\n";
         // sort(E.begin(), E.end(), [&](edge A, edge B) {return A.w < B.w;});
-        sort(E.begin(), E.end(), cmp);
+        sort(E, E+count, cmp);
 
         vector<double> D;
         for(edge e: E){
@@ -70,7 +71,7 @@ int main(){
                 Union(e.u, e.v);
             }
         }
-        printf("%.2lf\n", D[p-s-1]);
+        printf("%.2f\n", D[p-s-1]);
         n--;
     }
 
